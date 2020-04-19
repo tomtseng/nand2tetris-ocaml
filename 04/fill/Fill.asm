df66907c3c8509d9a3836ab09fff6ee40eb52e56
Tom Tseng <tom.hm.tseng@gmail.com>
2020-04-19 15:42:58 -0700

    04: Add solutions

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,41 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(RESET_CURRENT_SCREEN_WORD)
+@SCREEN
+D=A
+@current_screen_word
+M=D
+
+// Repeatedly write to `current_screen_word` based on keyboard input and
+// increment `current_screen_word`.
+(LOOP)
+
+@KBD
+D=M
+@WRITE_BLACK_WORD
+D;JNE
+
+(WRITE_WHITE_WORD)
+@current_screen_word
+A=M
+M=0
+@END_WRITE
+0;JMP
+
+(WRITE_BLACK_WORD)
+@current_screen_word
+A=M
+M=-1
+
+(END_WRITE)
+
+@current_screen_word
+M=M+1
+D=M
+@24576  // last screen word
+D=A-D;
+@RESET_CURRENT_SCREEN_WORD
+D;JLE
+@LOOP
+0;JMP
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,26 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// Clear R2.
+@2
+M=0
+
+(LOOP)
+// Repeat this loop body R0 many times by decrementing R0 and breaking out of
+// the loop when R0 becomes negative.
+@0
+M=M-1;
+D=M
+@END
+D;JLT
+
+// Add R1 to R2.
+@1
+D=M
+@2
+M=M+D
+
+@LOOP
+0;JMP
+
+(END)
